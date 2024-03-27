f2747738a94c414e5ceca628eb6e402bcacb6a6f
Alipa <alipakkr24@gmail.com>
2024-03-27 17:18:39 +0530

    frontend

---
+++ frontend/src/data/index.js
@@ -0,0 +1,12 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Alipa',
+    lastName: 'Ranga',
+    email: 'alipakkr24@example.com',
+    salary: '26000',
+    date: '24-12-2001'
+  }
+];
+
+export { employeesData };
